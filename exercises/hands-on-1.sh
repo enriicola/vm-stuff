b773f4fe1a38e24b41104c5fda5e1c09bff57b04
vagrant <enrico.pezzano@outlook.it>
2023-10-18 21:15:12 +0000

    enrico's script to auto push

--- exercises/hands-on-1.sh
+++ exercises/hands-on-1.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# Configure Vagrant to automatically enable SSH access using your personal key generate ssh key on the host machine (or use your personal one)
+    # generate ssh key on the host machine (or use your personal one)
+    # use a file provisioner to copy the public key on /tmp/id.pub
+    # use a script provisioner (setup_pubkey.sh) to append the above key on the authorized_keys of the user vagrant
+    # use a configuration variable KEY_FILE to specify the file containing the public key
+    # check if the key already exists in the authorized_keys before adding it
+    # test whether it works with an ssh client and vscode
+
+# for loop to create file 1 to 10
+for i in {1..10}
+do
+    touch file$i.txt
+done
+
